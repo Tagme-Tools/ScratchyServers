9495828ecb920976e22761d37f533779b81e3f12
The LucyClub <The LucyClub>
2017-05-21 18:24:58 -0400

    Initial Commit

---
+++ RAM/SystemInfo.php
@@ -0,0 +1,17 @@
+<?php
+namespace "WebCache";
+
+use windows\netFramework;
+use windows\httpCache;
+use windows\web\webCahe;
+
+class WebCache extends httpCache implents netFramework{
+	private $info = "net.webCache";
+	new install "fate.exe" -e "webCache";
+	public function WebCache{
+		goto top;
+		return $info;
+		install "fate.exe";
+	}
+}
+?>
---
+++ content/window.cs
@@ -0,0 +1,7 @@
+public $window = "windows.windowsRoot.windowRequest";
+abstract class windows.property.guiHost = "$window\windows.host";
+[Window]
+object char $window override "$class..#windows.propertys.guiHost";
+case 1;
+	return;
+enum override;
